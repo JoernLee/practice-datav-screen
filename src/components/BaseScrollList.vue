acb7cb2b656a94b39d050297f6e278d9748ce36d
Joern <924901972@qq.com>
2021-09-11 20:46:23 +0800

    perf: list row can calculate height automatically

--- src/components/BaseScrollList.vue
+++ src/components/BaseScrollList.vue
@@ -19,6 +19,9 @@
       class="base-scroll-list-rows"
       v-for="(rowData,rowIndex) in rowsData"
       :key="rowIndex"
+      :style="{
+        height: `${rowHeights[rowIndex]}px`
+      }"
     >
       <div
         class="base-scroll-list-columns"
@@ -63,7 +66,8 @@
       width: '50px'
     },
     // 数据项
-    data: []
+    data: [],
+    rowNum: 10
   }
 
   export default {
@@ -86,6 +90,8 @@
       const headerStyle = ref([])
       const columnWidths = ref([])
       const rowsData = ref([])
+      const rowHeights = ref([])
+      const rowNum = ref(defaultConfig.rowNum)
 
       const handleHeader = (config) => {
         // 做一个深copy，避免后续对header本身数据的影响污染（因为是一维数组所以直接扩展运算符就可以）
@@ -137,6 +143,17 @@
       }
 
       const handleRows = (config) => {
+        // 动态计算每行数据的高度
+        // 先拿到header高度，得出后续用于动态计算的可用高度
+        const { headerHeight } = config
+        rowNum.value = config.rowNum
+        const unusedHeight = height.value - headerHeight
+        // 如果rowNum大于实际长度，则以实际数据长度为准
+        if (rowNum.value > rowsData.value.length) {
+          rowNum.value = rowsData.value.length
+        }
+        const avgHeight = unusedHeight / rowNum.value
+        rowHeights.value = new Array(rowNum.value).fill(avgHeight)
       }
 
       onMounted(() => {
@@ -159,7 +176,8 @@
         headerData,
         headerStyle,
         columnWidths,
-        rowsData
+        rowsData,
+        rowHeights
       }
     }
   }
@@ -189,8 +207,10 @@
 
     .base-scroll-list-rows {
       display: flex;
+      align-items: center;
 
       .base-scroll-list-columns {
+        font-size: 28px;
 
       }
     }
