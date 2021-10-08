22d82e6d5f96c638b50ec48e2269614e10567e7c
Joern <924901972@qq.com>
2021-10-08 20:06:54 +0800

    feat:finish columns data adapt

--- src/components/SalesList/index.vue
+++ src/components/SalesList/index.vue
@@ -21,6 +21,7 @@
       const update = () => {
         const data = []
         const headerIndexData = []
+        const aligns = []
         // 数据处理逻辑 - 一共六列算上序号列
         for (let i = 0; i < props.data.length; i++) {
           data[i] = []
@@ -33,6 +34,33 @@
             headerIndexData[i] = `<div style="width:100%;height:100%;display:flex;align-items:center;justify-content:center;">
             <div style="width:15px;height:15px;background:rgb(38,88,104);border-radius:50%;border:1px solid #fff;"/>
           </div>`
+          }
+          for (let j = 0; j < 5; j++) {
+            aligns.push('center')
+            let text = ''
+            switch (j) {
+              case 0:
+                text = props.data[i].order
+                break
+              case 1:
+                text = props.data[i].shop
+                break
+              case 2:
+                text = props.data[i].rider
+                break
+              case 3:
+                text = props.data[i].newShop
+                break
+              case 4:
+                text = props.data[i].avgOrder
+                break
+            }
+            if (j === 1 || j === 3) {
+              // 这两列text为绿色 - 这样可以实现不同列的文字颜色
+              data[i].push(`<div style="color: rgb(178,209,126)">${text}</div>`)
+            } else {
+              data[i].push(`<div>${text}</div>`)
+            }
           }
         }
         config.value = {
@@ -48,9 +76,10 @@
           rowBg: ['rgb(40,40,40)', 'rgb(55,55,55)'],
           rowFontSize: 24,
           rowColor: '#fff',
-          aligns: ['center', 'center', 'center'],
+          aligns,
           data
         }
+        console.log(config)
       }
       onMounted(() => {
         // 父组件的onMounted是在子组件都mounted完成之后进行的，所以只是这样处理列表子组件是没办法更新拿到config的
