dbfefc13cd806deda7c989aca53b3556a491c370
Joern <924901972@qq.com>
2021-11-08 21:16:18 +0800

    feat: test visual map component

--- src/components/JiangsuMapWarning/index.vue
+++ src/components/JiangsuMapWarning/index.vue
@@ -19,9 +19,28 @@
         fetch('http://www.youbaobao.xyz/datav-res/datav/jiangsuMapData.json')
           .then(response => response.json())
           .then(data => {
+            console.log(data)
+            const center = []
+            data.features.forEach(item => {
+              if (item.properties) {
+                center.push({
+                  [item.properties.name]: item.properties.center
+                })
+              }
+            })
+            console.log('城市中心点坐标', center)
             // 注册地图 - options中才可以引用
             ECharts.registerMap('jiangsu', data)
             options.value = {
+              visualMap: {
+                show: true, // 控制左下角标识
+                max: 100,
+                seriesIndex: [0], // 如果下面添加了多组series数据就需要配置
+                // 自定义深浅分布颜色
+                inRange: {
+                  color: ['#a5dcf4', '#006edd']
+                }
+              },
               geo: [{
                 map: 'jiangsu', // 使用自定义地图
                 zoom: 1, // 初始倍率
@@ -39,6 +58,43 @@
                   shadowOffsetX: -5,
                   shadowOffsetY: 15
                 }
+              }],
+              series: [{
+                // 使用series-map类型来构建精细化地图
+                type: 'map',
+                mapType: 'jiangsu',
+                zoom: 1,
+                roam: false,
+                label: {
+                  show: true,
+                  color: '#fff',
+                  emphasis: {
+                    color: '#fff',
+                    show: false
+                  }
+                },
+                itemStyle: {
+                  normal: {
+                    borderColor: '#2980b9',
+                    borderWidth: 1,
+                    areaColor: '#12235c'
+                  },
+                  // 鼠标悬浮时
+                  emphasis: {
+                    areaColor: '#fa8c16',
+                    borderWidth: 0
+                  }
+                },
+                data: center.map(item => {
+                  // item = {'南京市':[123,321]}
+                  // value可以给visualMap来进行视图映射
+                  const key = Object.keys(item)[0]
+                  const value = Math.random() * 100 // 生成0-100w随机人口数
+                  return {
+                    name: key,
+                    value
+                  }
+                })
               }]
             }
           })
